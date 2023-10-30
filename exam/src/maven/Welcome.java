ffc6932778b8ce74034bc4c462c24f915f252560
yagneswar19 <146819570+yagneswar19@users.noreply.github.com>
2023-10-30 12:07:18 +0530

    Update Welcome.java

--- exam/src/maven/Welcome.java
+++ exam/src/maven/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("hello welcome ");
 		System.out.print("anthor new line");
+		Sytem.out.println("git hub");
 
 	}
 
